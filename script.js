1c4597cebbc1a638bfd645d650ec6c108be5b0f0
afrobug <afrojahmed6@gmail.com>
2023-04-18 19:28:49 +0530

    newQuote

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+
 let apiQuotes = [];
 
 function newQuote() {
-   const randomIndex = Math.floor(Math.random() * apiQuotes.length);
-}
+   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    quoteText.textContent = quote.text;
+    if(!quote.author) {
+        authorText.textContent = "unknown";
+    } else {
+        authorText.textContent = quote.author;
+    }
+    
+} 
 
 async function getQuotes() {
     const apiUrl = "https://type.fit/api/quotes";
     try{
         const response = await fetch(apiUrl);
-        const apiQuotes = await response.json();
+        apiQuotes = await response.json();
         newQuote();
     } catch (error){
 
